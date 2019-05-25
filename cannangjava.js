c93018024af3a4993547a34e0eb2950818eb2db9
huynhvantu <huynhvantunlp@gmail.com>
2019-05-25 16:56:02 +0700

    first commit

---
+++ cannangjava.js
@@ -0,0 +1,17 @@
+function xuly(){
+	console.log('da vao ham')
+	var cannang = document.getElementById('socannang').value;
+	var chieucao = document.getElementById('sochieucao').value;
+	var BMI = parseInt(cannang)/(parseFloat(chieucao)*parseFloat(chieucao));
+	console.log('BMI')
+	if(BMI<18){
+		ketqua = "underweight";
+	}else if (BMI<25.0){
+			ketqua = "normal"
+	}else if (BMI<30.0){
+			ketqua = "Overweight";
+	}else{
+		ketqua="Obese";
+	}
+	document.getElementById('ketquala').innerHTML="chi so can nang la: " +ketqua;
+}
